6a0aa9d6572ce4bedebe5aafd4564d3cb23c42c4
Dyapa Sai Akshara <137030356+21wh1a0533@users.noreply.github.com>
2025-06-23 09:33:51 +0530

    Update settings.py

--- invoiceProject/settings.py
+++ invoiceProject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vg4dj#5h9#g-bd!=c0vfnle59!))9c#x36zdut-$#d_hwum&8v'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -139,7 +139,7 @@ EMAIL_HOST_PASSWORD = 'wyzb hgxn mqnl ctof'
 
 
 
-PERPLEXITY_API_KEY = "AIzaSyDwiZBIVVyBaZrr6c5HtB4L8kr-EDRzOdE"
+PERPLEXITY_API_KEY = ""
 
 
 # # settings.py
